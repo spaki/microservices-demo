524c5e0c2a8fe1560c3fb4b3880cf7ff3fba5439
Eduardo Spaki <eduardo@code21.com.br>
2020-11-27 12:32:30 -0300

    error handling

--- MSD.ZipCode.V1/MSD.ZipCode.V1.API/Controllers/ZipCodeController.cs
+++ MSD.ZipCode.V1/MSD.ZipCode.V1.API/Controllers/ZipCodeController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using MSD.ZipCode.V1.API.Dto;
 using MSD.ZipCode.V1.API.Interfaces;
 using MSD.ZipCode.V1.API.Model;
+using System;
 using System.ServiceModel;
 using System.Web;
 
@@ -16,18 +18,26 @@ namespace MSD.ZipCode.V1.API.Controllers
         public ZipCodeController(AppSettings settings) => this.settings = settings;
 
         [HttpGet("{zipCode}")]
-        public Dto.Address Get(string zipCode)
+        public ActionResult<ApiDefaultResponse<Address>> Get(string zipCode)
         {
-            zipCode = HttpUtility.UrlDecode(zipCode);
-
-            using (var channel = new ChannelFactory<IAtendeCliente>(new BasicHttpBinding(BasicHttpSecurityMode.Transport), new EndpointAddress(settings.CorreiosWS)))
+            try
             {
-                var client = channel.CreateChannel();
-                var data = client.consultaCEP(new consultaCEP(zipCode));
-                ((ICommunicationObject)client).Close();
-                channel.Close();
-                var result = new Address(data);
-                return result;
+                zipCode = HttpUtility.UrlDecode(zipCode);
+
+                using (var channel = new ChannelFactory<IAtendeCliente>(new BasicHttpBinding(BasicHttpSecurityMode.Transport), new EndpointAddress(settings.CorreiosWS)))
+                {
+                    var client = channel.CreateChannel();
+                    var data = client.consultaCEP(new consultaCEP(zipCode));
+                    ((ICommunicationObject)client).Close();
+                    channel.Close();
+                    var entity = new Address(data);
+
+                    return Ok(new ApiDefaultResponse<Address>(entity, true, null));
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ApiDefaultResponse<Address>(null, false, ex.ToString()));
             }
         }
     }
---
+++ MSD.ZipCode.V1/MSD.ZipCode.V1.API/Dto/ApiDefaultResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace MSD.ZipCode.V1.API.Dto
+{
+    public class ApiDefaultResponse<T>
+    {
+        public ApiDefaultResponse(T payload, bool success, string message)
+        {
+            Payload = payload;
+            Success = success;
+            Message = message;
+        }
+
+        public T Payload { get; private set; }
+        public bool Success { get; private set; }
+        public string Message { get; private set; }
+    }
+}
